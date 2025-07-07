93544e22c5874290cd2a8396b434dd59de8433b5
Tejas Shetty <jshettytejas@gmail.com>
2025-07-07 09:29:29 +0530

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>SGPA Calculator</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <meta name="description" content="SGPA Calculator for Sahyadri College students" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
