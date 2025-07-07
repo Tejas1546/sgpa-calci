2c70cf238962749c2282565e4807890cfb665885
Tejas Shetty <jshettytejas@gmail.com>
2025-07-07 10:00:55 +0530

    optimised for mobile view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <meta name="description" content="SGPA Calculator for Sahyadri College students" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
+        <meta name="theme-color" content="#2c3e50" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="SGPA Calculator" />
+        <meta name="mobile-web-app-capable" content="yes" />
       </Head>
       <Component {...pageProps} />
       <Analytics />
