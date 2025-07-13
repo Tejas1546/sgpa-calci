38c5f007a7128b5fb9107c3c97b7902d6b112101
Tejas Shetty <jshettytejas@gmail.com>
2025-07-14 00:16:20 +0530

    fix

--- types/index.ts
+++ types/index.ts
@@ -13,7 +13,7 @@ export interface CalculationResult {
   totalPoints: number;
 }
 
-export type Department = 'CSE' | 'ISE' | 'ECE' | 'MRE';
+export type Department = 'CSE' | 'ISE' | 'ECE' | 'EEE' | 'ME' | 'CE';
 export type Semester = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
 export type SubBranch = 'DS' | 'ISE';
 export type Cycle = 'Physics Cycle' | 'Chemistry Cycle'; 
